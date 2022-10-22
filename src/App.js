1d77b9b74780897d2b0b6b6e66f6578ee0b8fed9
Rahul Ekbote <rahulek@gmail.com>
2022-10-22 10:47:51 +0530

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="titleBar">
+        <h2>ReactJS Hooks Demo</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
